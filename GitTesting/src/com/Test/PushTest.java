9e4c97cf79eca6abfce17f390065b493a192877d
pratibha.b.mishra <pratibha.b.mishra@BDC6-DX-5GGMXH2.dir.svc.accenture.com>
2017-07-04 19:07:09 +0530

    method added

--- GitTesting/src/com/Test/PushTest.java
+++ GitTesting/src/com/Test/PushTest.java
@@ -5,5 +5,9 @@ public class PushTest {
 	public static void main(String[] args) {
 		System.out.println("Push Test");
 	}
+	
+	public static void main(String[] args) {
+		System.out.println("Push Test by pratibha");
+	}
 
 }
