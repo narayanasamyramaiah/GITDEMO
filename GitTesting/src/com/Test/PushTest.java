fd69ac75b997f77551850436e39a6edda3c919b7
narayanasamy.ramaiah <narayanasamy.ramaiah@BDC3-LX-52971CX.dir.svc.accenture.com>
2017-07-04 19:15:27 +0530

    Commit The RecentChanges Ramaiah

--- GitTesting/src/com/Test/PushTest.java
+++ GitTesting/src/com/Test/PushTest.java
@@ -2,6 +2,17 @@ package com.Test;
 
 public class PushTest {
 	
+	
+	public void display(int a,int b) throws ArithmeticException
+	{
+		int c=a/b;
+		
+		throw new ArithmeticException("Cant Divide By Zero");
+		
+		
+		
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Push Test");
 	}
